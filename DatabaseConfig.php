80afc482ff07bd7f75a7484aac887752f7da8954
Ashwin-Saxena <ashwin.1822co1018@kiet.edu>
2019-07-15 21:23:03 +0530

    first

---
+++ DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "mysql10.000webhost.com";
+
+//Define your database username here.
+$HostUser = "id9710617_atodzbawai";
+
+//Define your database password here.
+$HostPass = "fckugfFMRO";
+
+//Define your database name here.
+$DatabaseName = "id9710617_atodzbawai";
+
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+<head><title>Holiday Inn</title>
+ <script>function alet(){alert("Data will be further added depending upon the content");}</script>
+<link href="style.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=IBM+Plex+Serif:400i&display=swap" rel="stylesheet">
+<style>
+.btn{
+  background-color:  white;/* Green */
+  border: none;
+  color: #4CAF50;
+
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+border: 2px solid green;
+border-radius: 4px;
+
+}
+</style>
+
+</head>
+<body>
+
+<img src="https://image.flaticon.com/icons/svg/54/54206.svg" onclick="alet()" width="60px" height="60px" style="margin-left:1%;margin-top: 2%;float:left">
+    <div style="font-family:Verdana;margin-left: 30%;text-align: center;float:left;color:Green;font-family: 'IBM Plex Serif', serif;margin-top:0%;font-size: 70px;text-shadow:10px 3px 10px">Holiday Inn</div>
+<div  style="margin-left:18%;float:left;margin-top:1%;"><button onclick="location.href='login.html'" class="btn" style="width:120px;height:50px" >Login</button></div>
+<div  style="margin-right: 1%;margin-left: 4%;float:left;margin-top:1%;" ><button onclick="location.href='signup.php'" class="btn" style="width:120px;height:50px" >Signup</button></div>
+  <br><br><br><br><br><br><br><br>
+<div style="width:100%;height:120%;margin-left: 0%;margin-right:0%;box-shadow: 0px 0px 40px 4px gray;background:url('https://ihg.scene7.com/is/image/ihg/holiday-inn-clark-4065310151-4x3');background-size: 100% 100%;animation:slideshow 8s infinite;animation-delay:1.5s">
+<br><br><br><br><br><br>
+
+</div>
+<br><br><br></body></html>
